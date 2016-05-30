9b8d1645052a34ae0eb4b6c700976ca6b99f3c2a
hheiniida <hheiniida@users.noreply.github.com>
2016-05-30 20:50:11 +0300

    Add files via upload

---
+++ hello_world_controller.php
@@ -0,0 +1,23 @@
+<?php
+
+  class HelloWorldController extends BaseController{
+
+    public static function index(){
+      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+   	  View::make('home.html');
+            //echo'Tämä on etusivu!';
+    }
+
+    public static function sandbox(){
+      // Testaa koodiasi täällä
+      //echo 'Hello World!';
+         View::make('helloworld.html');
+    }
+    public static function login(){
+        View::make('login.html');
+    }
+    public static function edit(){
+        View::make('edit.html');
+    }
+  }
+  ?>
