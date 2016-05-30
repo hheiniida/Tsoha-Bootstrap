833f53568e9acf216a2f5565ab43e89e2ebd418f
hheiniida <hheiniida@users.noreply.github.com>
2016-05-30 18:53:14 +0300

    Add files via upload

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,22 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Henkilo(
+  id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+  nimi varchar(50) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
+  salasana varchar(50) NOT NULL
+);
+
+CREATE TABLE Muistettava(
+  id SERIAL PRIMARY KEY,
+  muistettava_id INTEGER REFERENCES Henkilo(id), -- Viiteavain Henkilo-tauluun
+ luokka_id INTEGER REFERENCES Luokka(id),
+ nimi varchar(50) NOT NULL,
+  prioriteetti varchar(2),
+  kuvaus varchar(50),
+  pvm DATE
+);
+
+CREATE TABLE Luokka(
+id SERIAL PRIMARY KEY,
+nimi varchar(15) NOT NULL,
+kuvaus varchar(50)
+);
