44a7d8afebc5032030dc37f1315356818a429cdf
hheiniida <hheiniida@users.noreply.github.com>
2016-06-08 16:44:05 +0300

    Add files via upload

--- muistettava_controller.php
+++ muistettava_controller.php
@@ -15,6 +15,47 @@ class MuistettavaController extends BaseController {
         View::make('index.html');
     }
 
+    public static function edit($id) {
+        $muistettava = Muistettavat::find($id);
+        View::make('edit', array('attributes' => $muistettava));
+    }
+
+    // Pelin muokkaaminen (lomakkeen käsittely)
+    public static function update($id) {
+        $params = $_POST;
+
+        $attributes = array(
+            'nimi' => $params['nimi'],
+            'prioriteetti' => $params['prioriteetti'],
+            'kuvaus' => $params['kuvaus'],
+            'pvm' => $params['pvm']
+        );
+
+        // Alustetaan Game-olio käyttäjän syöttämillä tiedoilla
+        $muistettava = new Muistettavat($attributes);
+        $errors = $muistettava->errors();
+
+        if (count($errors) > 0) {
+            View::make('game/edit.html', array('errors' => $errors, 'attributes' => $attributes));
+        } else {
+            // Kutsutaan alustetun olion update-metodia, joka päivittää pelin tiedot tietokannassa
+            $muistettava->update();
+
+            Redirect::to('/:id/edit' . $muistettava->id, array('message' => 'Peliä on muokattu onnistuneesti!'));
+        }
+    }
+
+    // Pelin poistaminen
+    public static function destroy($id) {
+        // Alustetaan Game-olio annetulla id:llä
+        $muistettava = new Muistettavat(array('id' => $id));
+        // Kutsutaan Game-malliluokan metodia destroy, joka poistaa pelin sen id:llä
+        $muistettava->destroy();
+
+        // Ohjataan käyttäjä pelien listaussivulle ilmoituksen kera
+        Redirect::to('/:id/edit', array('message' => 'Peli on poistettu onnistuneesti!'));
+    }
+
     public static function store() {
         // POST-pyynnön muuttujat sijaitsevat $_POST nimisessä assosiaatiolistassa
 
@@ -29,12 +70,13 @@ class MuistettavaController extends BaseController {
             'kuvaus' => $params['kuvaus'],
             'pvm' => $params['pvm']
         ));
-
-        // Kutsutaan alustamamme olion save metodia, joka tallentaa olion tietokantaan
-        $muistettava->save();
-
-        // Ohjataan käyttäjä lisäyksen jälkeen pelin esittelysivulle
-        Redirect::to('/todo', array('message' => 'Muisettava on lisätty kirjastoosi!'));
+        if ($params['nimi'] != '' && strlen($params['nimi']) >= 3) {
+            $nimi->save();
+            Redirect::to('/todo', array('message' => 'Muisettava on lisätty kirjastoosi!'));
+            // ...
+        } else {
+            Redirect::to('/todo', array('message' => 'Nimessä oli virhe!'));
+        }
     }
 
 }
