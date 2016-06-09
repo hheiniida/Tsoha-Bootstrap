1dcdb0f9552119aced7bb52c067235726c5892a2
hheiniida <hheiniida@users.noreply.github.com>
2016-06-09 19:59:06 +0300

    Delete add_test_data.sql

--- add_test_data.sql
+++
@@ -1,6 +0,0 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
--- Henkilo-taulun testidata
-INSERT INTO Henkilo (nimi, salasana) VALUES ('Kalle', 'Kalle123'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
-INSERT INTO Henkilo (nimi, salasana) VALUES ('Henri', 'Henri123');
--- Muistettava taulun testidata
-INSERT INTO Muistettava (nimi, prioriteetti, kuvaus, pvm) VALUES ('Vie roskat', '6', 'Vie roskat keittiöstä ja olohuoneesta.', NOW());
