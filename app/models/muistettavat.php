e10cbe05c4f4613f121dfa6226822562ce5c3bcf
hheiniida <hheiniida@users.noreply.github.com>
2016-06-15 16:49:28 +0300

    Add files via upload

--- app/models/muistettavat.php
+++ app/models/muistettavat.php
@@ -1,16 +1,17 @@
 <?php
- 
+
 class Muistettavat extends BaseModel {
+
     public $id, $henkilo_id, $luokka_id, $nimi, $prioriteetti,
             $kuvaus, $pvm;
- 
+
     public function __construct($attributes) {
         parent::__construct($attributes);
     }
- 
+
     public static function all() {
         // Alustetaan kysely tietokantayhteydellämme
-        $query = DB::connection()->prepare('SELECT * FROM Muistettava');
+        $query = DB::connection()->prepare('SELECT * FROM Muistettava order by prioriteetti asc');
         // Suoritetaan kysely
         $query->execute();
         // Haetaan kyselyn tuottamat rivit
@@ -31,6 +32,7 @@ class Muistettavat extends BaseModel {
         }
         return $items;
     }
+
     public function save() {
         // Lisätään RETURNING id tietokantakyselymme loppuun, niin saamme lisätyn rivin id-sarakkeen arvon
         $query = DB::connection()->prepare('INSERT INTO Muistettava (nimi, prioriteetti, kuvaus, pvm) VALUES (:nimi, :prioriteetti, :kuvaus, :pvm) RETURNING id');
@@ -41,11 +43,12 @@ class Muistettavat extends BaseModel {
         // Asetetaan lisätyn rivin id-sarakkeen arvo oliomme id-attribuutin arvoksi
         $this->id = $row['id'];
     }
+
     public static function find($id) {
         $query = DB::connection()->prepare('SELECT * FROM Muistettava WHERE id = :id LIMIT 1');
         $query->execute(array('id' => $id));
         $row = $query->fetch();
- 
+
         if ($row) {
             return new Muistettavat(array(
                 'id' => $row['id'],
@@ -54,10 +57,25 @@ class Muistettavat extends BaseModel {
                 'nimi' => $row['nimi'],
                 'prioriteetti' => $row['prioriteetti'],
                 'kuvaus' => $row['kuvaus'],
-                'pvm' => $row['pvmpublisher']
+                'pvm' => $row['pvm']
             ));
         }
- 
+
         return null;
     }
+
+    public static function delete($id) {
+
+        $query = DB::connection()->prepare('DELETE FROM Muistettava WHERE id = :id');
+        $query->execute(array('id' => $id));
+    }
+
+    public function update($id) {
+        $query = DB::connection()->prepare('UPDATE Muistettava SET id=:id, nimi=:nimi, luokka_id=:luokka_id, prioriteetti=:prioriteetti, kuvaus=:kuvaus, pvm=:pvm WHERE id = :id');
+        $query->execute(array('id' => $id, 'nimi'=> $this->nimi,'luokka_id'=> $this->luokka_id, 'prioriteetti'=> $this->prioriteetti, 'kuvaus'=> $this->kuvaus, 'pvm'=> $this->pvm));
+        $row = $query->fetch();
+
+        Kint::dump($row);
+    }
+
 }
