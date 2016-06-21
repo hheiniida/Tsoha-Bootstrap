07c45cf875d9e3939a0e2b12d0f60e668c08a45b
hheiniida <hheiniida@users.noreply.github.com>
2016-06-21 14:41:45 +0300

    Add files via upload

--- app/models/user.php
+++ app/models/user.php
@@ -8,10 +8,31 @@
 
 class User extends BaseModel {
 
-    public $henkilo_id, $nimi;
+    public $id, $nimi, $salasana;
 
     public static function find() {
         return 'id';
+    }
+
+    public function authenticate($nimi, $salasana) {
+        $query = DB::connection()->prepare('SELECT * FROM Henkilo WHERE nimi = :nimi AND salasana = :salasana LIMIT 1');
+        $query->execute(array('nimi' => $nimi, 'salasana' => $salasana));
+        $row = $query->fetch();
+        
+        if ($row) {
+            
+            // Käyttäjä löytyi, palautetaan löytynyt käyttäjä oliona
+            return new User(array(
+                'id'=> $row['id'],
+                'nimi'=> $row['nimi'],
+                'salasana'=> $row['salasana']
+            ));
+                
+        
+        } else {
+            // Käyttäjää ei löytynyt, palautetaan null
+            return null;
+        }
     }
 
 }
